372b05e852cd9f9259733b73c52fda9073059606
StephaniEngell <stephaniengel18@gmail.com>
2025-06-24 21:18:25 -0300

    insert global styles

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 export type ButtonVariant = "primary" | "secondary" | "danger" | "success";
 
@@ -6,20 +6,9 @@ interface ButtonContainerProps {
   variant: ButtonVariant;
 }
 
-const buttonVariants = {
-  primary: "purple",
-  secondary: "orange",
-  success: "green",
-  danger: "red",
-};
-
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
-  ${(props) => {
-    return css`
-      background-color: ${buttonVariants[props.variant]};
-    `;
-  }}
+  background-color: ${(props) => props.theme.primary};
 `;
